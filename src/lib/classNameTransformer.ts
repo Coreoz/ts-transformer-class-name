5dac68808c284fa5d589789c2cb05552763ee24e
Aurélien Manteaux <amanteaux@coreoz.com>
2023-01-23 19:07:14 +0100

    Use yarn and fix dependencies

--- src/lib/classNameTransformer.ts
+++ src/lib/classNameTransformer.ts
@@ -9,33 +9,34 @@ export function classNameTransformer(): ts.TransformerFactory<ts.SourceFile> {
         return (sourceFile: ts.SourceFile): ts.SourceFile => {
             const visitor = (node: ts.Node): ts.Node | undefined => {
                 if (ts.isClassLike(node)) {
-                    const updatedClassMembers: readonly ts.ClassElement[] = [
-                        ...(node.members),
-                        context.factory.createGetAccessorDeclaration(
-                            undefined,
-                            [
-                                context.factory.createModifier(ts.SyntaxKind.PublicKeyword),
-                                context.factory.createModifier(ts.SyntaxKind.StaticKeyword),
-                            ],
-                            context.factory.createComputedPropertyName(
-                                context.factory.createIdentifier(
-                                    `Symbol.for("${Injector.CONSTRUCTOR_NAME_SYMBOL_IDENTIFIER}")`
-                                )
-                            ),
-                            [],
-                            undefined,
-                            context.factory.createBlock([
-                                context.factory.createReturnStatement(
-                                    context.factory.createStringLiteral(node.name ? node.name.getFullText().trim() : 'undefined')
-                                ),
-                            ])
-                        ),
-                    ];
                     if (ts.isClassDeclaration(node)) {
+                        const updatedClassMembers: readonly ts.ClassElement[] = [
+                            ...(node.members),
+                            context.factory.createGetAccessorDeclaration(
+                                undefined,
+                                [
+                                    context.factory.createModifier(ts.SyntaxKind.PublicKeyword),
+                                    context.factory.createModifier(ts.SyntaxKind.StaticKeyword),
+                                ],
+                                context.factory.createComputedPropertyName(
+                                    context.factory.createIdentifier(
+                                        `Symbol.for("${Injector.CONSTRUCTOR_NAME_SYMBOL_IDENTIFIER}")`
+                                    )
+                                ),
+                                [],
+                                undefined,
+                                context.factory.createBlock([
+                                    context.factory.createReturnStatement(
+                                        context.factory.createStringLiteral(node.name ? node.name.getFullText().trim() : 'undefined')
+                                    ),
+                                ])
+                            ),
+                        ];
+
                         return context.factory.updateClassDeclaration(
                             node,
                             node.decorators,
-                            node.modifiers,
+                            node.modifiers?.filter(ts.isModifier),
                             node.name,
                             node.typeParameters,
                             node.heritageClauses,
